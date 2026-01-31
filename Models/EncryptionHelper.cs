7758c3911c326f92999238571470c69c321872af
Umutalb <Umutalb@users.noreply.github.com>
2026-01-31 20:17:24 +0300

    Adds encryption helper
    Adds an encryption helper class with methods for encrypting and decrypting strings using AES encryption with a SHA256 key derivation.
    
    This ensures secure handling of sensitive data.

--- Models/EncryptionHelper.cs
+++ Models/EncryptionHelper.cs
@@ -1,6 +1,68 @@
-﻿namespace SelfDestructMessageAPI.Models
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SelfDestructMessageAPI.Models
 {
-    public class EncryptionHelper
+    public static class EncryptionHelper
     {
+        public static string Encrypt(string plainText, string key)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                using (var sha256 = SHA256.Create())
+                {
+                    aes.Key = sha256.ComputeHash(Encoding.UTF8.GetBytes(key));
+                }
+
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(array);
+        }
+
+        public static string Decrypt(string cipherText, string key)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                using (var sha256 = SHA256.Create())
+                {
+                    aes.Key = sha256.ComputeHash(Encoding.UTF8.GetBytes(key));
+                }
+
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                            // a
+                        }
+                    }
+                }
+            }
+        }
     }
 }
